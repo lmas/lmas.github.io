56d70bcc04722e4037013c0898ba16252c703194
Alex <github@larus.se>
2023-08-23 15:13:41 +0200

    Cleans up copyright year filter

--- .filters/copyright-year.lua
+++ .filters/copyright-year.lua
@@ -1,14 +1,14 @@
+-- Adds correct copyright year to a post's meta, if it has a date.
+
 function Meta(meta)
-    if meta.date then
-        local format = "(%d+)-(%d+)-(%d+)"
-        local y, m, d = pandoc.utils.stringify(meta.date):match(format)
-        local date = os.time({
-            year = y,
-            month = m,
-            day = d,
-        })
-        local date_string = os.date("%Y", date)
-        meta.copyright_year = pandoc.MetaString(date_string)
+    if not meta.date then
         return meta
     end
+
+    local format = "(%d+)-(%d+)-(%d+)"
+    local y, m, d = pandoc.utils.stringify(meta.date):match(format)
+    local date = os.time({year = y, month = m, day = d})
+    local date_string = os.date("%Y", date)
+    meta.copyright_year = pandoc.MetaString(date_string)
+    return meta
 end
